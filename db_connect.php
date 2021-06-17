db2317b86378977ee29393bba6485dfcdff9eb8f
srtech-3077 <sunilyadav120472@gmail.com>
2021-06-17 11:39:04 +0530

    init

---
+++ db_connect.php
@@ -0,0 +1,11 @@
+<?php
+
+$conn= mysqli_connect('sql302.epizy.com', 'epiz_27755317', 'Sunil3077', 'epiz_27755317_Education');
+
+/*if ($db){
+    echo "success";
+ }
+ else
+    die("Error". mysqli_connect_error());
+*/
+?>
